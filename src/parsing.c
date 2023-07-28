3a53d66d44c80913948b642b083acbcd8216adc0
minjcho <117441212+minjcho@users.noreply.github.com>
2023-07-28 16:49:27 +0900

    split after splited by |

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: minjcho <minjcho@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 14:06:50 by minjcho           #+#    #+#             */
-/*   Updated: 2023/07/28 16:41:11 by minjcho          ###   ########.fr       */
+/*   Updated: 2023/07/28 16:47:38 by minjcho          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,14 @@ void	parsing(t_mini *mini, char *line)
 	int	i = 0;
 	while (splited_pipe[i])
 	{
-		printf("%d : %s\n", i, splited_pipe[i]);
+		printf("i : %d\n", i);
+		mini[i].command = ft_split2(splited_pipe[i], ' ');
+		int j = 0;
+		while (mini[i].command[j])
+		{
+			printf("%s\n", mini[i].command[j]);
+			j++;
+		}
 		i++;
 	}
 }
